fd9bb1c9ea3a0e98bac4c3ade1cc908b29f16b28
QuantumCoder <157535720+HanzlaCoding@users.noreply.github.com>
2026-03-10 00:37:41 +0500

    ...

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('limo-v1').then(cache =>
-      cache.addAll(['/', '/styles.min.css'])
+      cache.addAll(['/', '/index.html', '/script.min.js'])
     )
   );
 });
